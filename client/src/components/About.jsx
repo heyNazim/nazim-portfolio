3f1968ac4aa87e7dea6f85034c181b0b41435048
heyNazim <nazim.saifi1908@gmail.com>
2024-03-30 21:16:15 +0530

    about

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -16,25 +16,25 @@ function About() {
     config: { mass: 5, tension: 300, friction: 60 },
   }));
 
-  
+
 
   const skills = [
-    {name:"HTML",val:"90"},
-    {name:"CSS",val:"80"},
-    {name:"Bootstrap",val:"92"},
-    {name:"Material UI",val:"85"},
-    {name:"Tailwind",val:"90"},
-    {name:"JavaScript",val:"80"},
-    {name:"React",val:"80"},
-    {name:"NodeJs",val:"70"},
-    {name:"ExpressJS",val:"70"},
-    {name:"MongoDB",val:"80"},
-    {name:"MySql",val:"70"},
+    { name: "HTML", val: "90" },
+    { name: "CSS", val: "80" },
+    { name: "Bootstrap", val: "92" },
+    { name: "Material UI", val: "85" },
+    { name: "Tailwind", val: "90" },
+    { name: "JavaScript", val: "80" },
+    { name: "React", val: "80" },
+    { name: "NodeJs", val: "70" },
+    { name: "ExpressJS", val: "70" },
+    { name: "MongoDB", val: "80" },
+    { name: "MySql", val: "70" },
     // {name:"React Native",val:"70"},
   ]
 
   return (
-   
+
     <section id="about" className="about">
       <div className="container" data-aos="fade-up">
         <div className="section-title">
@@ -55,16 +55,15 @@ function About() {
           <div className="col-lg-8 pt-4 pt-lg-0 content">
             <h3>Full Stack Web Developer &amp; Freelancer</h3>
             <p className="fst-italic">
-              My name Nazim Saifi, I am a Full Stack Web Developer and
-              Web Designer. An Open Source enthusiast and a Computer Science
-              student from India.I love working on new and exciting technologies
-              emerging nowadays.I have good work experience as a MERN Stack
-              Developer in startup(s) and UI/UX Designer where I was core member
-              of the development team and done quite some contribution to open
-              source as well. :Skills- React.js Next.js Node.js Express.js
-              Mongodb MySql  JavaScript Material UI, Bootstrap,
-              Tailwind.css Html Css
+
+              My name is Nazim Saifi. I am a Full Stack Web Developer and Web Designer, as well as an Open Source enthusiast and a Computer Science student from India. I am passionate about working on new and exciting technologies emerging today. With significant experience as a MERN Stack Developer in startups and as a UI/UX Designer, I have been a core member of development teams and have made substantial contributions to open source projects.
+
             </p>
+            <p className="fst-italic">
+              My skills include React.js, Next.js, Node.js, Express.js, MongoDB, MySQL, JavaScript, Material UI, Bootstrap, Tailwind.css, HTML, and CSS.
+
+            </p>
+
 
             <div className="row">
               <div className="col-lg-6">
@@ -105,7 +104,7 @@ function About() {
                     <strong>City:</strong> <span>Noida</span>
                   </li>
                 </ul>
-                
+
               </div>
               <div className="col-lg-6 d-flex  align-items-center">
                 <a href={resume} className="button-58" download>
@@ -114,37 +113,37 @@ function About() {
                   </span>
                 </a>
               </div>
-              
+
             </div>
           </div>
 
-         
+
         </div>
         <div id="skills" className="skills section-bg my-5">
-      <div className="container">
-        <div className="section-title">
-          <h2>Skills</h2>
-          <p>I love working on new and exciting technologies emerging nowadays.I have good work experience as a MERN Stack Developer in startup(s) and UI/UX Designer where I was core member of the development team and done quite some contribution to open source as well</p>
-        </div>
-        <div className="row skills-content">
-          {skills.map((item)=>{
-            return <div className="col-lg-6"key={item.name}>
-             <div className="progress" >
-              <span className="skill">{item.name} <i className="val">{item.val}%</i></span>
-              <div className="progress-bar-wrap" style={{borderRadius:"5px"}}>
-              <ProgressBar completed={item.val} height="10px" isLabelVisible={false} transitionDuration="3s" animateOnRender={true} />
-              </div>
+          <div className="container">
+            <div className="section-title">
+              <h2>Skills</h2>
+              <p>I love working on new and exciting technologies emerging nowadays.I have good work experience as a MERN Stack Developer in startup(s) and UI/UX Designer where I was core member of the development team and done quite some contribution to open source as well</p>
+            </div>
+            <div className="row skills-content">
+              {skills.map((item) => {
+                return <div className="col-lg-6" key={item.name}>
+                  <div className="progress" >
+                    <span className="skill">{item.name} <i className="val">{item.val}%</i></span>
+                    <div className="progress-bar-wrap" style={{ borderRadius: "5px" }}>
+                      <ProgressBar completed={item.val} height="10px" isLabelVisible={false} transitionDuration="3s" animateOnRender={true} />
+                    </div>
+                  </div>
+                </div>
+              })}
             </div>
           </div>
-          })}
         </div>
       </div>
-    </div>
-      </div>
-      
-    
+
+
     </section>
-   
+
   );
 }
 
